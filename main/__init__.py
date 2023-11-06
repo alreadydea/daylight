b812e0992d0a84f8eff2a4d5cf49921b5a3e7abd
Spy <119489004+amthespy@users.noreply.github.com>
2023-11-06 18:20:01 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,13 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+# variables
+API_ID = 24490919
+API_HASH = "d1b3b15126c47dd4cb491553ee1db910"
+BOT_TOKEN = "6354397130:AAFCed0ol-7QpddRJ8zRWs-JJWaZx3XKkK4"
+SESSION = "BQF1s6cAT1We76J_xvM9cauuSCenGLnQPVJVDuHtVdSksfrLIVhszIlmRybGG3Cajl-8PGFYBxjr7mevY9v09WZh9w9LNnbg2GXRVZX7k60x6C1_GK1C0Xxv9f6hWXB-kuxW13eOR9I_2sp7dJYA3UAYteWEkjWlp4G1EvvtE87z0cZMTY9H-5va8FEP5F0pypFI9zPwuUUOZECCVz16YZANrkzqb6V-qtgOM1aPuObml3Il4d6cYCqaL76tD6lq-CHZXW0xx3pf6NWRDbSBqqQ24r9KTae_d3aVi-rxmFFcuKjJYzHrNrLMn8ho5aMX0ldKEHSimGlWvNAON5r5LBqPNnSrGgAAAAF0xr8GAA"
+FORCESUB = "devgagan"
+AUTH = 5621114370
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
