6fd10da0c5a33dc1ba27222ce261a0bfa61368a4
Spy <119489004+amthespy@users.noreply.github.com>
2023-11-18 07:43:20 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -1,10 +1,3 @@
-#Tg:MaheshChauhan/DroneBots
-#Github.com/Vasusen-code
-
-"""
-Plugin for both public & private channels!
-"""
-
 import time, os, asyncio
 
 from .. import bot as Drone
@@ -26,25 +19,19 @@ ft = f"To use this bot you've to join @{fs}."
 
 batch = []
 
-@Drone.on(events.NewMessage(incoming=True, from_users=AUTH, pattern='/cancel'))
+@Drone.on(events.NewMessage(incoming=True, pattern='/cancel'))
 async def cancel(event):
-    if not event.sender_id in batch:
+    if not event.chat_id in batch:
         return await event.reply("No batch active.")
     batch.clear()
     await event.reply("Done.")
     
-@Drone.on(events.NewMessage(incoming=True, from_users=AUTH, pattern='/batch'))
-async def _batch(event):
-    if not event.is_private:
-        return
-    s, r = await force_sub(event.client, fs, event.sender_id, ft) 
-    if s == True:
-        await event.reply(r)
-        return       
-    if event.sender_id in batch:
+@Drone.on(events.NewMessage(incoming=True,  pattern='/batch'))
+async def _batch(event):   
+    if event.chat_id in batch:
         return await event.reply("You've already started one batch, wait for it to complete you dumbfuck owner!")
     async with Drone.conversation(event.chat_id) as conv: 
-        if s != True:
+       # if s != True:
             await conv.send_message("Send me the message link you want to start saving from, as a reply to this message.", buttons=Button.force_reply())
             try:
                 link = await conv.get_reply()
@@ -66,18 +53,18 @@ async def _batch(event):
                 return conv.cancel()
             try:
                 value = int(_range.text)
-                if value > 100:
+                if value > 10000:
                     await conv.send_message("You can only get upto 100 files in a single batch.")
                     return conv.cancel()
             except ValueError:
                 await conv.send_message("Range must be an integer!")
                 return conv.cancel()
-            batch.append(event.sender_id)
-            await run_batch(userbot, Bot, event.sender_id, _link, value) 
+            batch.append(event.chat_id)
+            await run_batch(userbot, Bot, event.chat_id, _link, value) 
             conv.cancel()
             batch.clear()
 
-async def run_batch(userbot, client, sender, link, _range):
+async def run_batch(userbot, client, event.chat_id, link, _range):
     for i in range(_range):
         timer = 60
         if i < 25:
@@ -92,22 +79,22 @@ async def run_batch(userbot, client, sender, link, _range):
             else:
                 timer = 3
         try: 
-            if not sender in batch:
-                await client.send_message(sender, "Batch completed.")
+            if not event.chat_id in batch:
+                await client.send_message(event.chat_id, "Batch completed.")
                 break
         except Exception as e:
             print(e)
-            await client.send_message(sender, "Batch completed.")
+            await client.send_message(event.chat_id, "Batch completed.")
             break
         try:
-            await get_bulk_msg(userbot, client, sender, link, i) 
+            await get_bulk_msg(userbot, client, event.chat_id, link, i) 
         except FloodWait as fw:
             if int(fw.x) > 299:
-                await client.send_message(sender, "Cancelling batch since you have floodwait more than 5 minutes.")
+                await client.send_message(event.chat_id, "Cancelling batch since you have floodwait more than 5 minutes.")
                 break
             await asyncio.sleep(fw.x + 5)
-            await get_bulk_msg(userbot, client, sender, link, i)
-        protection = await client.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
+            await get_bulk_msg(userbot, client, event.chat_id, link, i)
+        protection = await client.send_message(event.chat_id, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
         await asyncio.sleep(timer)
         await protection.delete()
             
